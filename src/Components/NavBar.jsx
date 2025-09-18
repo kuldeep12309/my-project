4ece0ab55cfec315e7feca8e6669f18343fa0ad4
Kuldeep Gupta <kuldeepgupta8800@gmail.com>
2025-09-18 16:27:07 +0530

    "commit"

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
@@ -5,18 +6,40 @@ import { Link } from "react-router-dom";
 import Offcanvas from "react-bootstrap/Offcanvas";
 
 function NavBar() {
-  const expand = 'md'; 
+  const expand = "md";
+
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => {
+    setShow(false);
+    window.scrollTo({ top: 0, behavior: "smooth" }); // Scroll page to top smoothly
+  };
+
+  const handleShow = () => setShow(true);
 
   return (
-    <Navbar expand={expand} bg="primary" data-bs-theme="dark" className="container-fluid">
+    <Navbar
+      expand={expand}
+      bg="primary"
+      data-bs-theme="dark"
+      className="container-fluid"
+    >
       <Container fluid>
         <Navbar.Brand>I Portfolio</Navbar.Brand>
-        <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
-        <Navbar.Offcanvas className="bg-primary"
+
+        <Navbar.Toggle
+          aria-controls={`offcanvasNavbar-expand-${expand}`}
+          onClick={handleShow}
+        />
+
+        <Navbar.Offcanvas
+          show={show}
+          onHide={handleClose}
+          className="bg-primary"
           id={`offcanvasNavbar-expand-${expand}`}
           aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
           placement="start"
-          style={{ width: '50%' }} 
+          style={{ width: "50%" }}
         >
           <Offcanvas.Header closeButton className="text-white">
             <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
@@ -28,16 +51,36 @@ function NavBar() {
               className="ms-auto my-2 my-lg-0 justify-content-end flex-grow-1 pe-3"
               navbarScroll
             >
-              <Nav.Link as={Link} to="/" className="text-white">
+              <Nav.Link
+                as={Link}
+                to="/"
+                className="text-white"
+                onClick={handleClose}
+              >
                 Header
               </Nav.Link>
-              <Nav.Link as={Link} to="/about" className="text-white">
+              <Nav.Link
+                as={Link}
+                to="/about"
+                className="text-white"
+                onClick={handleClose}
+              >
                 About
               </Nav.Link>
-              <Nav.Link as={Link} to="/resume" className="text-white">
+              <Nav.Link
+                as={Link}
+                to="/resume"
+                className="text-white"
+                onClick={handleClose}
+              >
                 Resume
               </Nav.Link>
-              <Nav.Link as={Link} to="/contact" className="text-white">
+              <Nav.Link
+                as={Link}
+                to="/contact"
+                className="text-white"
+                onClick={handleClose}
+              >
                 Contact
               </Nav.Link>
             </Nav>
