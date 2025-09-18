d0d55a8d5dd21e0a4be98299256337476f4a008f
Kuldeep Gupta <kuldeepgupta8800@gmail.com>
2025-09-18 13:36:39 +0530

    "commit"

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,33 +1,43 @@
 import React from "react";
 const About = () => {
   const skills = [
-    "HTML5", "CSS3", "JavaScript (ES6+)", "React.js", "Redux Toolkit",
-    "React Router", "Context API", "React Hook Form", "Tailwind CSS",
-    "Bootstrap", "Fetch API", "Git & GitHub", "Responsive Design",
+    "HTML5",
+    "CSS3",
+    "JavaScript (ES6+)",
+    "React.js",
+    "Redux Toolkit",
+    "React Router",
+    "Context API",
+    "React Hook Form",
+    "Tailwind CSS",
+    "Bootstrap",
+    "Fetch API",
+    "Git & GitHub",
+    "Responsive Design",
   ];
 
   return (
-    <div className=" py-5 about-section" >
+    <div className=" py-5 about-section m-4">
       <h1 className="text-center mb-5">About Me</h1>
 
       <div className="row  mb-5">
-        <div>
+        <div className="m-2 ">
           <p className="text-center">
-            Hello! I'm <strong>Kuldeep Gupta</strong>, a dedicated and enthusiastic{" "}
-            <strong>Frontend Developer</strong> passionate about crafting clean, responsive,
-            and user-friendly web interfaces. I specialize in building scalable React applications
-            using modern tools like Redux Toolkit, Tailwind CSS, and React Hooks. My goal is to
-            continuously learn and stay updated with the latest in frontend development while
-            delivering high-quality web experiences.
+            Hello! I'm <strong>Kuldeep Gupta</strong>, a dedicated and
+            enthusiastic <strong>Frontend Developer</strong> passionate about
+            crafting clean, responsive, and user-friendly web interfaces. I
+            specialize in building scalable React applications using modern
+            tools like Redux Toolkit, Tailwind CSS, and React Hooks. My goal is
+            to continuously learn and stay updated with the latest in frontend
+            development while delivering high-quality web experiences.
           </p>
         </div>
       </div>
 
-      <div className="mt-5 ">
-        <h2 className="text-center mb-5 ">My Frontend Skills</h2>
+      <div className="mt-5">
+        <h2 className="text-center mb-5">My Frontend Skills</h2>
 
-
-        <div className="d-flex flex-wrap justify-content-center gap-3  ">
+        <div className="d-flex flex-wrap justify-content-center gap-3">
           {skills.map((skill, index) => (
             <span
               key={index}
